a6350b507d258a58eafb9b729e3d95e5a67ba49c
Andrej5768 <109480576+Andrej5768@users.noreply.github.com>
2022-07-24 17:24:30 +0300

    Update Sorter.java

--- src/MyApp/Sorter.java
+++ src/MyApp/Sorter.java
@@ -41,6 +41,54 @@ public class Sorter {
         }
     }
 
+    public static void selectionSort(int[] array) {
+        for (int left = 0; left < array.length; left++) {
+            int minInd = left;
+            for (int i = left; i < array.length; i++) {
+                if (array[i] < array[minInd]) {
+                    minInd = i;
+                }
+            }
+            swap(array, left, minInd);
+        }
+    }
+
+    public static void quickSort(int[] source, int leftBorder, int rightBorder) {
+        int leftMarker = leftBorder;
+        int rightMarker = rightBorder;
+        int pivot = source[(leftMarker + rightMarker) / 2];
+        do {
+            // Двигаем левый маркер слева направо пока элемент меньше, чем pivot
+            while (source[leftMarker] < pivot) {
+                leftMarker++;
+            }
+            // Двигаем правый маркер, пока элемент больше, чем pivot
+            while (source[rightMarker] > pivot) {
+                rightMarker--;
+            }
+            // Проверим, не нужно обменять местами элементы, на которые указывают маркеры
+            if (leftMarker <= rightMarker) {
+                // Левый маркер будет меньше правого только если мы должны выполнить swap
+                if (leftMarker < rightMarker) {
+                    int tmp = source[leftMarker];
+                    source[leftMarker] = source[rightMarker];
+                    source[rightMarker] = tmp;
+                }
+                // Сдвигаем маркеры, чтобы получить новые границы
+                leftMarker++;
+                rightMarker--;
+            }
+        } while (leftMarker <= rightMarker);
+
+        // Выполняем рекурсивно для частей
+        if (leftMarker < rightBorder) {
+            quickSort(source, leftMarker, rightBorder);
+        }
+        if (leftBorder < rightMarker) {
+            quickSort(source, leftBorder, rightMarker);
+        }
+    }
+
     private static void swap(int[] array, int ind1, int ind2) {
         int tmp = array[ind1];
         array[ind1] = array[ind2];
